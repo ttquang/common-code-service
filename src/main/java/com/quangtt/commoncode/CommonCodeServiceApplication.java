9e89911bfe36c22c40429167dea7d2064e6d862b
quangtt90 <ttquang90@gmail.com>
2023-07-30 23:23:50 +0700

    init

---
+++ src/main/java/com/quangtt/commoncode/CommonCodeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.quangtt.commoncode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonCodeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonCodeServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/quangtt/commoncode/CommonCodeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quangtt.commoncode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonCodeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
